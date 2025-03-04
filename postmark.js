f1225620096493097d4cc75dc2780e985498fb38
BenjaminAguirre <benjamin@ongrid.run>
2025-03-04 12:40:57 -0300

    add

--- postmark.js
+++
@@ -1,6 +0,0 @@
-import * as postmark from "postmark";
-
-const serverToken = process.env.POSTMARK_TOKEN;
-const client = new postmark.ServerClient(serverToken);
-
-export { client };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="es"> 
       <Head>
+      
       </Head>
       <body>
         <Main /> 
